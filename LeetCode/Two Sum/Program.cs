1f44c3996ec53812f89640d10f23d490d2745023
Lars Passic <43658463+larspassic@users.noreply.github.com>
2021-06-28 23:50:49 -0700

    Working on TwoSum

---
+++ LeetCode/Two Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] solution = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int secondIndex = 0;
+
+                while (nums[i + 1])
+                {
+                    secondIndex = i + 1;
+                }
+
+
+                if (nums[i] + nums[secondIndex] == target)
+                {
+                    solution[0] = i;
+                    solution[1] = i + 1;
+                }
+            }
+
+            return solution;
+        }
+    }
+}

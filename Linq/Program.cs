8579946d57790810308a1e55a417de2ad92558c9
Lars Passic <larspassic@outlook.com>
2021-07-06 17:01:26 -0700

    Working on C# 201 YouTube playlist with Linq samples

---
+++ Linq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqQueryStructure();
+
+
+
+        }
+
+        private static void LinqQueryStructure()
+        {
+            List<int> numbers = new List<int>{ 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var lowNums = from num in numbers
+                          where num < 5
+                          select num;
+
+            Console.WriteLine("Numbers < 5:");
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

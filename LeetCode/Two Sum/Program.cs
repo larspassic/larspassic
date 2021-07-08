dea0bdc17939a1798db8c48297cd7cb4d5fd416a
Lars Passic <larspassic@outlook.com>
2021-07-07 17:43:33 -0700

    Worked on solution to TwoSum problem

--- LeetCode/Two Sum/Program.cs
+++ LeetCode/Two Sum/Program.cs
@@ -14,28 +14,50 @@ namespace Two_Sum
 
            
             int[] solution = TwoSum(nums, target);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            //Output the result
+            Console.WriteLine($"The numbers from that set which add up to {target} are {solution[0]} and {solution[1]}");
         }
 
         public static int[] TwoSum(int[] nums, int target)
         {
             int[] result = new int[2];
 
+            //Outer loop to loop through the elements of the array
             for (int i = 0; i < nums.Length; i++)
             {
-                int secondIndex = 0;
+                bool resultFound = false;
 
-                while ((i + 1) < nums.Length)
+                //For each element of the array, we walk through and compare the outer element of the array to every other element of the array
+                for (int i2 = 0; i2 < nums.Length; i2++)
                 {
-                    secondIndex = i + 1;
+                    
+                    //If i and i2 are going to compare the same index - skip the comparison
+                    while (i != i2)
+                    {
+                        //Check to see if the two numbers work and assign them to the result array
+                        if (nums[i] + nums[i2] == target)
+                        {
+                            result[0] = i;
+                            result[1] = i2;
+
+                            resultFound = true;
+                        }
+                    }
+
+                }
+
+                if (resultFound)
+                {
+                    break;
                 }
 
 
-
-                if (nums[i] + nums[secondIndex] == target)
-                {
-                    result[0] = i;
-                    result[1] = i + 1;
-                }
             }
 
             return result;

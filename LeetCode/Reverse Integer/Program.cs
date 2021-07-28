924f06eb4e402bd594a190098a8d0bc980c53201
Lars Passic <larspassic@outlook.com>
2021-07-27 22:46:03 -0700

    Working on Reverse Integer
    This solution is not going to work, because I'm converting in and out of string. I will need to find a way to do this while only using numbers.

---
+++ LeetCode/Reverse Integer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Reverse_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Please enter an integer to reverse:");
+
+            string userInputString = Console.ReadLine();
+
+            int userInputInt = int.Parse(userInputString);
+
+            Solution myClass = new Solution();
+            
+            int finalResult = myClass.Reverse(userInputInt);
+
+            Console.WriteLine($"The final result was {finalResult}");
+        }
+    }
+
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            int result = 0;
+
+            string normalString = x.ToString();
+
+            string reversedString = null;
+
+            for (int i = 1; i <= normalString.Length; i++)
+            {
+                reversedString += normalString[normalString.Length - i];
+            }
+
+            result = int.Parse(reversedString);
+
+            if (result > int.MaxValue || result < int.MinValue)
+            {
+                result = 0;
+            }
+            
+            return result;
+        }
+    }
+}

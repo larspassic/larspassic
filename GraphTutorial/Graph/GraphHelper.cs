e5fcc740b00482f4c016f8180e9e454f726602fb
Lars Passic <larspassic@outlook.com>
2021-08-17 19:01:42 -0700

    Started "Build .NET Core apps with Microsoft Graph" Tutorial
    Got authentication working
    
    https://docs.microsoft.com/en-us/graph/tutorials/dotnet-core

---
+++ GraphTutorial/Graph/GraphHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using TimeZoneConverter;
+
+namespace GraphTutorial.Graph
+{
+    public class GraphHelper
+    {
+        private static DeviceCodeCredential tokenCredential;
+        private static GraphServiceClient graphClient;
+
+        public static void Initialize(
+            string clientId,
+            string[] scopes,
+            Func<DeviceCodeInfo, CancellationToken, Task> callBack)
+        {
+            tokenCredential = new DeviceCodeCredential(callBack, clientId);
+            graphClient = new GraphServiceClient(tokenCredential, scopes);
+        }
+
+        public static async Task<string> GetAccessTokenAsync(string[] scopes)
+        {
+            var context = new TokenRequestContext(scopes);
+            var response = await tokenCredential.GetTokenAsync(context);
+            return response.Token;
+        }
+    }
+}
---
+++ GraphTutorial/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using GraphTutorial.Graph;
+
+namespace GraphTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core Graph Tutorial\n");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting program");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+
+            //Initialize Graph Client
+            GraphHelper.Initialize(appId, scopes, (code, cancellation) =>
+             {
+                 Console.WriteLine(code.Message);
+                 return Task.FromResult(0);
+             });
+
+            var accessToken = GraphHelper.GetAccessTokenAsync(scopes).Result;
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Please choose one of the following options:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Display access token");
+                Console.WriteLine("2. View this week's calendar");
+                Console.WriteLine("3. Add an event");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    //Set to invalid value to resolve the problem
+                    choice = -1;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        //Exit the program
+                        Console.WriteLine("Goodbye");
+                        break;
+                    case 1:
+                        //Display access token
+                        Console.WriteLine($"Access token: {accessToken}\n");
+
+                        break;
+                    case 2:
+                        //List the calendar
+
+                        break;
+                    case 3:
+                        //Create a new event
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+                
+
+            }
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            //Check the appConfig for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]))
+            {
+                return null;
+            }
+
+            return appConfig;
+        }
+    }
+}

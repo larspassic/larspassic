a3b0669db1493e36dff2cb443671f9b5aa0b449e
Lars Passic <larspassic@outlook.com>
2021-09-28 20:35:35 -0700

    Resolving issues and cleaning up structure

--- Dog Tracker App/DogTracker.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DogTracker.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ Dog Tracker App/DogTracker.Repository/DatabaseAccessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DogTracker.DogDatabase;
+
+namespace DogTracker.Repository
+{
+    public class DatabaseAccessor
+    {
+        static DatabaseAccessor()
+        {
+            Instance = new DogDbContext();
+        }
+
+        public static DogDbContect Instance { get; private set; }
+    }
+}

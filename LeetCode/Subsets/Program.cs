c78dcab39c8cf9d0911842d9b423c796f798398a
Lars Passic <larspassic@outlook.com>
2021-11-18 13:56:25 -0800

    Trying LeetCode problems

--- LeetCode/Subsets/Program.cs
+++ LeetCode/Subsets/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Brought in the full template from documentation
 
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +13,29 @@ namespace Subsets // Note: actual namespace depends on the project name.
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] nums = { 1, 2, 3};
+
+            Console.WriteLine(nums);
         }
+    }
+
+    //Do I need this?
+    public interface IList
+    {
+
     }
 
     public class Solution
     {
+        //This came directly from LeetCode
         public IList<IList<int>> Subsets(int[] nums)
         {
+            //Stuck setting up the problem
+            //Just trying to set up an empty object to store the result
+            IList<IList<int>>  result = new IList<IList<int>>;
 
+            return result;
         }
     }
 }
---
+++ LeetCode/Unique Paths/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Unique_Paths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+        }
+    }
+
+    public class Solution
+    {
+        
+        
+        public int UniquePaths(int m, int n)
+        {
+            int result = new int();
+
+
+
+
+            return result;
+        }
+    }
+}

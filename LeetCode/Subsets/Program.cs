7f2b9701b939b81842845538f6a1ee8e757cd361
Lars Passic <larspassic@outlook.com>
2021-11-17 20:23:32 -0800

    Starting on subsets code project

---
+++ LeetCode/Subsets/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Brought in the full template from documentation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Subsets // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+
+        }
+    }
+}

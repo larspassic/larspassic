6fab836d1d1312f76e798cd8efd8b91a6057ea46
Lars Passic <larspassic@outlook.com>
2021-11-19 23:36:27 -0800

    Set up another LeetCode problem.
    This one looks promising.

---
+++ LeetCode/Remove Nth Node from End of List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Remove_Nth_Node_from_End_of_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+    
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode result = null;
+
+            return result;
+        }
+    }
+}

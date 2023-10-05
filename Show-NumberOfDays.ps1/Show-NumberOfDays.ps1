3bc18407f87113bd21fb295fe0f51786f5e58803
Lars Passic <larspassic@outlook.com>
2023-10-05 13:30:00 -0700

    Update Show-NumberOfDays.ps1
    Enhance script with new options, readability, and contextual, readable dates.

--- Show-NumberOfDays.ps1/Show-NumberOfDays.ps1
+++ Show-NumberOfDays.ps1/Show-NumberOfDays.ps1
@@ -2,11 +2,16 @@
 #Popup the number of days since a specific date.
 
 #Store the start dates as variables
-$dateDecember = Get-Date 12/6/2022
+$dateDecember = Get-Date 12/6/2022 
 $dateFebruary = Get-Date 02/08/2023
+$dateToday = Get-Date
 
-#Store today's date as a variable
-$today = Get-Date
+#Format the display dates
+#Do this by splitting the string on the space character, then keeping the first 4 substrings of the result
+$dateTodayDisplay = $dateToday.DateTime.Split(" ")[,0+1+2+3]
+$dateDecemberDisplay = $dateDecember.DateTime.Split(" ")[,0+1+2+3]
+$dateFebruaryDisplay = $dateFebruary.DateTime.Split(" ")[,0+1+2+3]
+
 
 #Subtract the start date from today's date to compute and output the number of days passed
 $daysDecember = ($today - $dateDecember).Days
@@ -15,5 +20,14 @@ $daysFebruary = ($today - $dateFebruary).Days
 #Create the shell object
 $wshell = New-Object -ComObject Wscript.Shell
 
+#Create the string object that will be used in the body of the popup message
+$strText = @"
+Today is $dateTodayDisplay.
+
+It has been $daysDecember days since $($dateDecemberDisplay).
+
+It has been $daysFebruary days since $($dateFebruaryDisplay).
+"@
+
 #Pop up the shell object to the user
-$wshell.Popup("It has been $daysDecember days since REDACTED.`n`nIt has been $daysFebruary days since REDACTED.",0,"Number of days",0x1)
+$wshell.Popup($strText,0,"Show-NumberOfDays.ps1",0x0 + 0x40)

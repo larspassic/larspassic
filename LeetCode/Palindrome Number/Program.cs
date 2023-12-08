f2f1f5c365e5386e9ae95379fc4b2eed89f2bd25
Lars Passic <larspassic@outlook.com>
2023-12-08 13:01:51 -0800

    Update Program.cs
    Wrote possible solution on paper in real life. Then wrote comments to help me visualize the possible solution.

--- LeetCode/Palindrome Number/Program.cs
+++ LeetCode/Palindrome Number/Program.cs
@@ -6,13 +6,34 @@
         {
             //Main is where you stage/prepare the solution
             //Main is handled by Leetcode, so no need to send it back to Leetcode
+            //Use main if you want to test multiple example inputs
 
+            //Example 1
+            int x = 121;
 
+            //Example 2
+            //int x = -121;
+
+            //Example 3
+            //int x = 10;
+
+            //Call the function and store the result as a bool
+            bool solution = IsPalindrome(x);
         }
 
         //This function which returns a bool was provided by Leetcode
-        public bool IsPalindrome(int x)
+        public static bool IsPalindrome(int x)
         {
+            //Paste everything inside this function into Leetcode to check work
+
+            //Create variables to hold regular and reversed results
+
+            //Convert x from integer in to a string
+
+            //Compare regular and reversed variables
+
+                //If they are the same, return true because Palindrome
+                //If they are not the same, return false because not Palindrome
 
         }
     }

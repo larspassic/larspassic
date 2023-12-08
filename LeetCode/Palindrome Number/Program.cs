ae24ecdc76b8bd8103bcb85fb4f260bd0a87c17b
Lars Passic <larspassic@outlook.com>
2023-12-08 11:02:09 -0800

    Set up Palindrome Number problem

---
+++ LeetCode/Palindrome Number/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main is where you stage/prepare the solution
+            //Main is handled by Leetcode, so no need to send it back to Leetcode
+
+
+        }
+
+        //This function which returns a bool was provided by Leetcode
+        public bool IsPalindrome(int x)
+        {
+
+        }
+    }
+}

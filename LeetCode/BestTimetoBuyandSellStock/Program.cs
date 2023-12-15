b5a2c60b2f3173a0b954cc14a2730abd3ac3c389
Lars Passic <larspassic@outlook.com>
2023-12-15 13:49:16 -0800

    Set up new easy difficulty problem.

---
+++ LeetCode/BestTimetoBuyandSellStock/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BestTimetoBuyandSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            /*
+            Example 1:
+            Input: prices = [7,1,5,3,6,4]
+            Output: 5
+            Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+            Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+            
+            Example 2:
+            Input: prices = [7,6,4,3,1]
+            Output: 0
+            Explanation: In this case, no transactions are done and the max profit = 0.
+            */
+
+            int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+
+            int solution = MaxProfit(prices[]);
+        }
+
+        public int MaxProfit(int[] prices)
+        {
+            int result = 0;
+
+
+
+            return result;
+        }
+    }
+}

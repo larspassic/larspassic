b2d246b8b8cc721b2c49997193f901e979a4149d
Lars Passic <larspassic@outlook.com>
2023-12-16 00:23:48 -0800

    Keep getting time limit exceeded on leetcode.
    Tried to initialize j as i+1 to save on wasted loops.
    
    Tried to skip losses to save on wasted loops.
    
    Tried removing superfluous comparisons to save.
    
    Still only passing 201 out of 212 test cases on leetcode.
    
    May need to use a different approach.

--- LeetCode/BestTimetoBuyandSellStock/Program.cs
+++ LeetCode/BestTimetoBuyandSellStock/Program.cs
@@ -23,6 +23,8 @@
             int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
 
             int solution = MaxProfit(prices);
+
+            Console.WriteLine(solution);
         }
 
         public static int MaxProfit(int[] prices)
@@ -31,16 +33,61 @@
             int result = 0;
 
             //Set up a variable called "highestProfit" 
-
+            int highestProfit = 0;
+            
+            
             //Loop through prices array
+            for (int i = 0; i < prices.Length; i++)
+            {
+                int subtractionResult = new int();
 
-            //Subtract i+1 from i
+                for (int j = i+1; j < prices.Length; j++)
+                {
+                    //If j is less than or equal to i, use continue to skip to the next iteration of the 'j' for loop.
+                    //This will help prevent selling stocks in the past.
+                    //What if I just assign j to be i+1 so I don't have to keep continuing past wasteful iterations?
+                    /*
+                    if (j <= i)
+                    {
+                        continue;
+                    }
+                    */
 
-            //Store the result in highestProfit
+                    //Trying to speed this up by doing some testing prior to storing the result in subtractionResult.
+                    //If the sell date has a lower price than the buy date, going to be a loss, skip the whole loop.
+                    if (prices[j] <= prices[i])
+                    {
+                        continue;
+                    }
 
-            //If the result is negative, or smaller than highestProfit, do nothing. Leave highestProfit as is.
+                    //Subtract j from i    //This is resulting in "buy high and sell low" results. Will need to fix.
+                    subtractionResult = prices[j] - prices[i];
 
-            //If the result is higher than highestProfit, replace highestProfit with current result
+                    //If the result is negative, or smaller than highestProfit, do nothing. Leave highestProfit as is.
+                    //Since I have eliminated losses above,
+                    //and I am implicitly filtering out values that are lower than highestProfit below,
+                    //I will try to comment this section out completely to save processing time.
+
+                    /*
+                    if (subtractionResult < 0 || subtractionResult < highestProfit)
+                    {
+                        //Use the continue keyword to skip the remainder of this 'j' for loop iteration
+                        continue;
+                    }
+                    */
+
+                    //If the result is higher than highestProfit, replace highestProfit with current result
+                    if (subtractionResult > highestProfit)
+                    {
+                        highestProfit = subtractionResult;
+                    }
+
+                }
+
+            }
+
+            //At the very end, assign highestProfit to result
+            result = highestProfit;
 
 
             //Send the result back.

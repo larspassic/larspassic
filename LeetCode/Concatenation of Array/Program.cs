ef6057479fa2d156aed8f070c593b2739ea898d7
Lars Passic <larspassic@outlook.com>
2024-02-16 19:04:50 -0800

    Create a new Easy difficulty problem "Concatenation of Array"
    This one was fun! Finished it in the first sitting!

---
+++ LeetCode/Concatenation of Array/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Concatenation_of_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] nums = [1, 2, 1];
+            var ans = GetConcatenation;
+        }
+
+        public static int[] GetConcatenation(int[] nums)
+        {
+            //Create a variable that will store the result
+            int[] result = new int[(nums.Length*2)];
+
+            //Create some variables to use inside the for loop
+            bool hasConcatenated = false;
+            int resultIndex = 0;
+
+            //Loop through 
+            for (int i = 0; i < result.Length; i++)
+            {
+                //Check to see if i is already at the end
+                if (i == nums.Length)
+                {
+                    //Reset i to 0
+                    i = 0;
+                    
+                    //Keep track of if we've done this
+                    hasConcatenated = true;
+                }
+
+                //Store each result
+                result[resultIndex] = nums[i];
+
+                //Iterate the result index
+                resultIndex++;
+
+                if (resultIndex == result.Length)
+                {
+                    break;
+                }
+            }
+
+            //Send the result back
+            return result;
+        }
+    }
+}

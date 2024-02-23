dd5b94afe08ff05d152d5a312af74322cadf083f
Lars Passic <larspassic@outlook.com>
2024-02-23 15:39:58 -0800

    Create Easy difficulty Leetcode problem
    Noscoped "Defang an IP address"

---
+++ LeetCode/Defanging an IP Address/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Defanging_an_IP_Address
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string inputIP = "127.0.0.1";
+
+            string result = DefangIPaddr(inputIP);
+
+            Console.WriteLine(result);
+        }
+
+        public static string DefangIPaddr(string address)
+        {
+            string defangedAddress = null;
+
+            defangedAddress = address.Replace(".", "[.]");
+
+            return defangedAddress;
+        }
+    }
+}

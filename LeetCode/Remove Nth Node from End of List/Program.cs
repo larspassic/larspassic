7325b8080b9537e66a67232a1e7f49427dd1bd0f
Lars Passic <larspassic@outlook.com>
2024-09-27 17:56:49 -0700

    Good progress on Remove Nth Node from End of List
    Got 190 out of 208 test cases passed. Will continue working on it next week.

--- LeetCode/Remove Nth Node from End of List/Program.cs
+++ LeetCode/Remove Nth Node from End of List/Program.cs
@@ -2,13 +2,74 @@
 
 namespace Remove_Nth_Node_from_End_of_List
 {
-    internal class Program
+    public  class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            //head.next.next = new ListNode(3); 
+            //head.next.next.next = new ListNode(4); 
+            //head.next.next.next.next = new ListNode(5);
+
+            int n = 1;
+
+            ListNode displayResult = RemoveNthFromEnd(head, n);
         }
 
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            //Set up the result
+            ListNode result = head;
+
+            //Create a ListNode object that will be used to walk the list (listWalker?)
+            ListNode listWalker = result;
+
+            //Create a ListNode object that will be used to represent the new tail
+            //when we remove the n object from the list (newTail?)
+            ListNode newTail = null;
+
+            //Find the length of the list
+            int listNodeSize = 1;
+            ListNode listNodeCounter = head;
+
+            //Loop that will traverse the list and count the listNodeSize
+            while (listNodeCounter.next != null)
+            {
+                listNodeSize++;
+                listNodeCounter = listNodeCounter.next;
+            }
+
+            //Now that we know the size of the ListNode, only do this work if it's larger than 1
+            if (listNodeSize > 1)
+            {
+                //Move through the ListNode object until we reach ,
+                //to find the node prior to the one that needs to be removed.
+                for (int i = 1; i < (listNodeSize - n); i++)
+                {
+                    //Is this how I walk a list??
+                    listWalker = listWalker.next;
+                }
+
+                //Go forward in the list two times (.next.next?)
+                //which will be where the list picks up after n is removed.
+                newTail = listWalker.next.next;
+
+                //Set listWalker.next to be equal to newTail. Now we have removed n.
+                listWalker.next = newTail;
+
+            }
+            else
+            {
+                result = result.next;
+            }
+
+
+
+            return result;
+        }
 
     }
     
@@ -24,13 +85,4 @@ namespace Remove_Nth_Node_from_End_of_List
         }
     }
 
-    public class Solution
-    {
-        public ListNode RemoveNthFromEnd(ListNode head, int n)
-        {
-            ListNode result = null;
-
-            return result;
-        }
-    }
 }

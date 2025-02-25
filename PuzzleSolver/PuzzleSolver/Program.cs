08013275bdf03b542475d05c1457cbfd510ed413
Lars Passic <43658463+larspassic@users.noreply.github.com>
2025-02-24 23:46:13 -0800

    Trying to solve Murdle cypher puzzles using programming.
    And seeing what kind of Code Copilot can pump out!

---
+++ PuzzleSolver/PuzzleSolver/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the character sequence to try some ciphers:");
+            string userInput = Console.ReadLine();
+
+            Console.Write("Enter the number of iterations you want to try:");
+            int userInputInt = Convert.ToInt32(Console.ReadLine());
+
+            string inputString = userInput;
+
+            //Call the method the number of times you want to shift up
+            for (int i = 1; i <= userInputInt; i++)
+            {
+                //Shift the chars in the string by calling the method
+                string shiftedOutput = TryCiphers(inputString);
+
+                //Display the results
+                Console.WriteLine("When shifting by " + i + " the output is: " + shiftedOutput);
+
+                //Now set the output as the next input
+                inputString = shiftedOutput;
+            }
+
+
+        }
+
+        private static string TryCiphers(string userInput)
+        {
+            char[] shiftedChars = new char[userInput.Length];
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                char c = userInput[i];
+
+                //Only do this section if it's a real letter
+                if (char.IsLetter(c))
+                {
+                    //If it's lowercase z move it to lowercase a
+                    if (c == 'z')
+                    {
+                        shiftedChars[i] = 'a';
+                    }
+                    //If it's uppercase Z move it to uppercase A
+                    else if (c == 'Z')
+                    {
+                        shiftedChars[i] = 'A';
+                    }
+                    //Otherwise just move it up one char
+                    else
+                    {
+                        shiftedChars[i] = (char)(c + 1);
+                    }
+                }
+                else
+                {
+                    shiftedChars[i] = c;
+                }
+            }
+
+            return new string(shiftedChars);
+        }
+    }
+}
---
+++ SubstitutionCypherFinder/SubstitutionCypherFinder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string encryptedText = "zmobppf fxu tvbws bo mjjvoqv rxxt kdmwv bq kbpp bq fxu fxu iuqs mk xivo bor avsxpr xov pbqk kvttmzfmog qmgsk";
+        List<string> commonWords = new List<string> { "the", "you", "and", "is", "it", "an", "as" };
+
+        foreach (var permutation in GetPermutations("abcdefghijklmnopqrstuvwxyz", 26))
+        {
+            string decryptedText = Decrypt(encryptedText, permutation);
+            if (ContainsCommonWords(decryptedText, commonWords))
+            {
+                Console.WriteLine($"Possible solution with permutation: {new string(permutation)}");
+                Console.WriteLine(decryptedText);
+            }
+        }
+    }
+
+    static IEnumerable<char[]> GetPermutations(string characters, int length)
+    {
+        if (length == 1)
+            return characters.Select(t => new char[] { t });
+
+        return GetPermutations(characters, length - 1)
+            .SelectMany(t => characters, (t1, t2) => t1.Concat(new char[] { t2 }).ToArray());
+    }
+
+    static string Decrypt(string encryptedText, char[] substitution)
+    {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        Dictionary<char, char> cipher = new Dictionary<char, char>();
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            cipher[alphabet[i]] = substitution[i];
+        }
+
+        char[] decrypted = encryptedText.Select(c => cipher.ContainsKey(c) ? cipher[c] : c).ToArray();
+        return new string(decrypted);
+    }
+
+    static bool ContainsCommonWords(string text, List<string> commonWords)
+    {
+        return commonWords.Any(word => text.Contains(word));
+    }
+}
